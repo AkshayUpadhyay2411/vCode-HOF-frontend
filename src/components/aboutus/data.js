ff95b21e22b12cc8441d8d96282d1f8e78cdbf89
AkshayUpadhyay2411 <75607973+AkshayUpadhyay2411@users.noreply.github.com>
2022-06-03 17:04:29 +0530

    final1

---
+++ src/components/aboutus/data.js
@@ -0,0 +1,55 @@
+import { Button } from "bootstrap";
+
+const data = [
+    {
+        name: "Akshay",
+        // img: "https://drive.google.com/thumbnail?id=1f0GZNOrH4P2h31pHE_cBpIZULAuIasiw",
+        img: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+        role:"Web Developer",
+        linkedin: "https://www.linkedin.com/in/akshay-upadhyay-68981a200/",
+        mail: "lit2020008@iiitl.ac.in",
+        github: "https://github.com/AkshayUpadhyay2411"
+    },
+    {
+        name: "Aditya",
+        // img: "https://drive.google.com/thumbnail?id=1AuoKe3QnBxpA961cc7Or2izrMPRJ2Wp
+        
+        img: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+        role:"Web Developer",
+        linkedin: "https://www.linkedin.com/in/aditya-gupta-83426b237/",
+        mail: "lit2020055@iiitl.ac.in",
+        github: "https://github.com/adityastro"
+    },
+    {
+        name: "Kotiswar",
+        // img: "https://drive.google.com/thumbnail?id=1UJsfXtaw6snW7jXd0vjrNvzBTjI86vZZ",
+        
+        img: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+        role:"Web Developer",
+        linkedin: "https://www.linkedin.com/in/kaithepalli-kotiswar/",
+        mail: "twitter",
+        github: "https://github.com/Kkotiswar"
+    },
+    {
+        name: "Ankit",
+        // img: "https://drive.google.com/thumbnail?id=16oKSxy8XON1xhmG-mJEVrm-aTtP6D6Ug",
+        
+        img: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+        role: "Web Developer",
+        linkedin: "https://www.linkedin.com/in/ankit-chaudhary1/linkedin",
+        mail: "twitter",
+        github: "https://github.com/Traveller08"
+    },
+    {
+        name: "Anurag",
+        // img: "https://drive.google.com/thumbnail?id=10Oe-Os3NVxcut5N25Spr54VbuPVJLf1n",
+        
+        img: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+        role:"Web Developer",
+        linkedin: "https://www.linkedin.com/in/anurag-vats-999506215/",
+        mail: "twitter",
+        github: "https://github.com/AnuragVats007"
+    }
+];
+
+export default data;
---
+++ src/components/learn/learnDT.js
@@ -0,0 +1,68 @@
+
+const data = [
+    {
+        // url: "https://www.youtube.com/watch?v=1mhRZPYCkAY&list=PL2q4fbVm1Ik4JdzE2Bv_UUGBz0TXEIrai",
+        url: "https://youtu.be/zEAmQqOpfzM",
+        topic: "DSU"
+    },
+    {
+        // url: "https://www.youtube.com/watch?v=AfE1nZjG5ZM&list=PL2q4fbVm1Ik71hmKyay-GkU6J9GTXCe5g",
+        url: "https://www.youtube.com/watch?v=FfXoiwwnxFw&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY",
+        topic: "Dynamic Programming"
+    },
+    {
+        // url: "https://www.youtube.com/watch?v=foi9F4DfwXI&list=PL2q4fbVm1Ik7Ufc3rsyB3lkmQGL6vOgdR",
+        url: "https://www.youtube.com/watch?v=YTtpfjGlH2M&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw",
+        topic: "Graph & Trees"
+    },
+    {
+        // url: "https://www.youtube.com/watch?v=lvNyiyobb18&list=PL2q4fbVm1Ik7ALIK6Lu0uhCHYu-RS9m3H",
+        url: "https://www.youtube.com/watch?v=yVdKa8dnKiE&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9",
+        topic: "Recursion"
+    },
+    {
+        // url: "https://www.youtube.com/watch?v=hpXnxKwzaO8&list=PL2q4fbVm1Ik7Fz2JrP7meOtxUvEXy926-",
+        url: "https://www.youtube.com/watch?v=dBGUmUQhjaM&list=PLgUwDviBIf0pcIDCZnxhv0LkHf5KzG9zp",
+        topic: "Trie"
+    },
+    {
+        // url: "https://www.youtube.com/watch?v=VW85xQ6GJP4&list=PL2q4fbVm1Ik6DCzm9XZJbNwyHtHGclcEh",
+        url: "https://www.youtube.com/watch?v=nqowUJzG-iM&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go",
+        topic: "DP 2"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=OYqYEM1bMK8&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk",
+        topic: "Tree"
+    },
+    // {
+    //     url: "https://www.youtube.com/watch?v=efL86JCONH0&list=PL2q4fbVm1Ik7ip1VkWwe5U_CEb93vw6Iu",
+    //     topic: "Bit Manipulation"
+    // },
+    // {
+    //     url: "https://www.youtube.com/watch?v=FJXZ4Zt8SOk&list=PL2q4fbVm1Ik5Z3xcF-D0OgHv7QOvlokSm",
+    //     topic: "Implementation"
+    // },
+    {
+        url: "https://www.youtube.com/watch?v=TWMCMvfEAv4",
+        topic: "Linked List"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=xLetJpcjHS0&list=PLBlnK6fEyqRj9lld8sWIUNwlKfdUoPd1Y",
+        topic: "Data Structures"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=kepBmgvWNDw",
+        topic: "Recursions"
+    },
+    {
+        // url: "https://www.youtube.com/watch?v=foi9F4DfwXI&list=PL2q4fbVm1Ik7Ufc3rsyB3lkmQGL6vOgdR",
+        url: "https://www.youtube.com/watch?v=YTtpfjGlH2M&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw",
+        topic: "Graph & Trees"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=kepBmgvWNDw",
+        topic: "Recursions"
+    }
+];
+
+export default data;
